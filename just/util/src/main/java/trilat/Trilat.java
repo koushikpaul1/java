21e583da6a3e5a5a1f6b75bb61aa864cfc7ee3c4
edge <koushikpaul1@gmail.com>
2018-12-18 23:24:02 +0530

    trilat 2

--- just/util/src/main/java/trilat/Trilat.java
+++ just/util/src/main/java/trilat/Trilat.java
@@ -20,24 +20,23 @@ public class Trilat {
 		
 		
 		//double[][] positions = new double[][] { { 0.0, 0.0 }, {36.0,0.0 }, { 16.0, 32.0 },{2.0,00.0},{7.0,00.0} };
-		//double[] distances = new double[] { 29, 25, 13,20,13 };
+		//double[] distances = new double[] { 29, 25, 13,20,13 };	
 		
-		
-		
-
-		
-		/*double[][] positions = new double[][] { { 0.0, 0.0 },{ 2.23,1.5 }, { 4.46, 0.0 } } ;
-		double[] distances = new double[] { 4, 1.5, 4 };//2.23,3.1675742538227314 correct*/
-		
-
+		//1
 		//double[][] positions = new double[][] { { 2.0, 0.0 }, {5.0,0.0}, { 8.0, 0.0 } };
-		//double[] distances = new double[] { 4.24, 3, 4.24 };
-		
+		//double[] distances = new double[] { 4.24, 3, 4.24 };//5.0,0.0/wrong
+		//2
 		//double[][] positions = new double[][] { { 0,0 }, {2.5,1 }, { 5,0 } };
-		//double[] distances = new double[] { 4, 2.2, 4 };//2.4999999999999996,-1.7264197033284172
-		
-		//double[][] positions = new double[][] { { 2.0, 0.0 }, {5.0,0.0}, { 8.0, 0.0 } };
-		//double[] distances = new double[] { 4.24, 2.5, 4.24 };
+		//double[] distances = new double[] { 4, 2.2, 4 };//2.4999999999999996,-1.7264197033284172/wrong
+		//3
+		//double[][] positions = new double[][] { { 0.0, 0.0 }, {3.0,2.0}, { 6.0, 0.0 } };
+		//double[] distances = new double[] { 5, 2, 5};//3.0,4.0/correct
+		//4
+		//double[][] positions = new double[][] { { 0.0, 0.0 }, {3.0,1.5}, { 6.0, 0.0 } };
+		//double[] distances = new double[] { 5, 2.5, 5};//3.0,-1.6180333396289484/wrong
+		//5
+		//double[][] positions = new double[][] { { 0.0, 0.0 }, {3.0,1.5}, { 6.0, 0.0 } };
+		//double[] distances = new double[] { 5, 2.5, 5};//3.0,-1.6180333396289484/wrong
 		TrilaterationFunction trilaterationFunction=new TrilaterationFunction(positions,distances);
 		NonLinearLeastSquaresSolver nlSolver = new NonLinearLeastSquaresSolver(trilaterationFunction,new LevenbergMarquardtOptimizer());
 		Optimum optimum = nlSolver.solve();
