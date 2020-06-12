648cd1db0f787d01f6e24a05435e54c34472b71f
edge <edge@DESKTOP-P0AP89H>
2020-06-13 01:58:36 +0530

    13-6-20

--- coreJava/src/main/java/com/edge/collection/C_sort/CInsertionSort.java
+++ coreJava/src/main/java/com/edge/collection/C_sort/CInsertionSort.java
@@ -1,30 +1,18 @@
 package com.edge.collection.C_sort;
 
 public class CInsertionSort {
-    static int counter = 0;
-
-    public static void main(String[] args) {
-
-
-        //  int[] intArray = {20, 35, -15, 7, 55, 1, -22};
-        int[] intArray = {20, 19, 18, 17, 16, 15, 14, 13, 12, 10, 9, 8, 7, 6, 5, 4, 2, 1};
-
-        for (int i = 1; i < intArray.length; i++) {
-            int temp = intArray[i];
-
-            int j;
-
-            for (j = i; j > 0 && intArray[j - 1] > temp; j--) {
-                intArray[j] = intArray[j - 1];
-                counter++;
-            }
-
-            intArray[i] = temp;
-        }
-
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i]);
-        }
-        System.out.println(counter);
-    }
+	public static void main(String[] args) {
+		int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+		for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+			int newElement = intArray[firstUnsortedIndex];
+			int i;
+			for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+				intArray[i] = intArray[i - 1];
+			}
+			intArray[i] = newElement;
+		}
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+	}
 }
