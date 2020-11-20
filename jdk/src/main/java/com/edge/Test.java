8cf37f7e09c901623380b153b4e7d7cfaa01ff97
koushik <koushikpaul1@gmail.com>
2020-11-20 12:51:35 +0530

    refactor

---
+++ jdk/src/main/java/com/edge/Test.java
@@ -0,0 +1,12 @@
+package com.edge;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+	
+	public static void main (String a[]) {
+		List list = new LinkedList();
+	}
+
+}
