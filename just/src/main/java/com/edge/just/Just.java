215f440efb67ba3e864edb365b589e34f8c4bbf0
koushik <koushikpaul1@gmail.com>
2020-11-20 11:12:22 +0530

    java.io movement

--- just/src/main/java/com/edge/just/Just.java
+++ just/src/main/java/com/edge/just/Just.java
@@ -1,9 +1,13 @@
 package com.edge.just;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Just {
 
 	public static void main(String[] args) {
 		System.out.println("This is a holder for resources and infos");
+		List list = new LinkedList();
 
 	}
 
